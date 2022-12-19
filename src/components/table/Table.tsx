ca39c2fb4f088abcb0d355d5622b626c79b12409
vaishnavagrawal <39738820+vaishnavagrawal@users.noreply.github.com>
2022-12-19 19:58:58 +0530

    before antd

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
+    ColumnDef,
     createColumnHelper,
     flexRender,
     getCoreRowModel,
@@ -7,17 +8,67 @@ import {
   } from '@tanstack/react-table'
 import { useHospitals } from '../../hooks/useHospitals';
 import { date } from 'zod';
-  
-function Table() {
+import { ColumnsConfig } from '../../schemas/column';
+import { Table } from "@nextui-org/react";
+
+
+function TableRender() {
 
     const { status, data : hospitals , error, isFetching } = useHospitals();
+    const columnHelper = createColumnHelper<ColumnsConfig>()
 
+//     const columns : any  = useMemo<ColumnDef<ColumnsConfig, any>[]>(
+//         () => 
+//         [
+//             columnHelper.accessor('name', {
+//                 header : 'Name' ,
+//               footer: info => info.column,
 
-    console.log(hospitals)
+//             }),
+//             columnHelper.accessor(row => row.address, {
+//               id: 'lastName',
+//               cell: info => <i>{info.getValue()}</i>,
+//               header: () => <span>Last Name</span>,
+//               footer: info => info.column.id,
+//             }),
+//             // columnHelper.accessor('age', {
+//             //   header: () => 'Age',
+//             //   cell: info => info.renderValue(),
+//             //   footer: info => info.column.id,
+//             // }),
+//             // columnHelper.accessor('visits', {
+//             //   header: () => <span>Visits</span>,
+//             //   footer: info => info.column.id,
+//             // }),
+//             // columnHelper.accessor('status', {
+//             //   header: 'Status',
+//             //   footer: info => info.column.id,
+//             // }),
+//             // columnHelper.accessor('progress', {
+//             //   header: 'Profile Progress',
+//             //   footer: info => info.column.id,
+//             // }),
+//           ]
+//       ,
+//         []
+//       )
+    
+//   const tableData = useReactTable({
+//     hospitals  ,
+//     columns,
+//     // getCoreRowModel: getCoreRowModel(),
+//   })
+
+//   console.log('tableData' ,tableData.getHeaderGroups())
+
 
   return (
-    <div>Table</div>
+<div>
+    Table
+</div>
+
+
   )
 }
 
-export default Table
+export default TableRender
